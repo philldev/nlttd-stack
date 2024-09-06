4d07a5a599513d58f8d676410ddc2581d46c0cc4
Deddy Wolley <deddywolley@gmail.com>
2024-09-07 02:38:55 +0800

    add env

--- drizzle/db.ts
+++ drizzle/db.ts
@@ -1,11 +1,11 @@
 import { createClient } from "@libsql/client";
 import { drizzle } from "drizzle-orm/libsql";
-import { users, sessions } from "./schema";
+import { users, sessions, todos } from "./schema";
 
 // Setup sqlite database connection
 const client = createClient({
-  url: "file:sqlite.db",
+  url: process.env.DATABASE_URL ?? "file:./db.sqlite",
   authToken: process.env.DATABASE_AUTH_TOKEN,
 });
 
-export const db = drizzle(client, { schema: { users, sessions } });
+export const db = drizzle(client, { schema: { users, sessions, todos } });
